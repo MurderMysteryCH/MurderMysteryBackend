713da3ae3b403c09114667c0b62c0f2e8a684c67
Calvin Bonomo <cal.bonomo@gmail.com>
2026-01-21 13:37:12 -0700

    build works

--- backend/routers/game.py
+++ backend/routers/game.py
@@ -1,16 +1,18 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Query
 
 from ..models.game import BaseGame
 from ..models.player import GamePlayer
 
 router = APIRouter(
-  prefix='game'
+  prefix='/game'
 )
 
 @router.post('/create')
 def createGame(gameData: BaseGame):
   pass
 
-@router.post('/join')
-def joinGame(playerData: GamePlayer):
+@router.post('/join/{gameId}')
+def joinGame(gameId: Annotated[str, Query(max_length=6)], playerData: GamePlayer):
   pass
